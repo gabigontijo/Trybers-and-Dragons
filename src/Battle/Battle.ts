dd1b85d33b3fcb3ac9165d758a0fce18acfe6a12
Gabriela Gontijo <dev.gabigontijo@gmail.com>
2023-03-27 11:49:33 +0100

    req 6- cria a interface Fighter

--- src/Battle/Battle.ts
+++ src/Battle/Battle.ts
@@ -1,7 +1,7 @@
-// import Fighter from '../Fighter';
+import Fighter from '../Fighter';
 
 abstract class Battle {
-  constructor(protected player: any) { }
+  constructor(protected player: Fighter) { }
 
   // Should return 1 if player wins, -1 otherwise
   fight(): number {
